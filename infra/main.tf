cf6bd83c232a5c6f34490462fab21bf3ca8a3a16
EmmaDjeufa <57154645+EmmaDjeufa@users.noreply.github.com>
2023-10-25 17:20:40 +0200

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -2,18 +2,15 @@ provider "docker" {
   host = "tcp://localhost:2375" # Adresse de votre serveur Docker local
 }
 
-resource "docker_image" "build" {
-  name         = "fastapi-app:latest"
-  build        {
-    context = "../app"
+resource "docker_image" "redis" {
+  name         = "redis:latest"
+}
+
+resource "docker_container" "redis" {
+  name  = "redis-container"
+  image = docker_image.redis.name
+  ports {
+    internal = 6379 # Port Redis
   }
 }
 
-resource "docker_container" "container" {
-  name  = "fastapi-container"
-  image = docker_image.build.name
-  ports {
-    internal = 80
-    external = 8080
-  }
-}
