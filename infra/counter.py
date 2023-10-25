a3d2a31f92ec55824ddaf03b8afa7d0f9b284794
EmmaDjeufa <57154645+EmmaDjeufa@users.noreply.github.com>
2023-10-25 18:48:11 +0200

    Update counter.py

--- infra/counter.py
+++ infra/counter.py
@@ -1,8 +1,7 @@
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
-Si votre objectif est d'ouvrir le fichier HTML (`ShopifyShop.html`) à partir de votre script Python (`counter.py`), vous pouvez utiliser un module Python tel que `webbrowser` pour ouvrir le fichier HTML dans le navigateur par défaut de la machine où le script est exécuté. Voici comment vous pouvez le faire dans votre script Python :
 
-```python
+
 import webbrowser
 
 # Spécifiez le chemin vers le fichier HTML
@@ -10,4 +9,4 @@ html_file = "ShopifyShop.html"
 
 # Utilisez la fonction open de webbrowser pour ouvrir le fichier HTML
 webbrowser.open(html_file)
-```
+
