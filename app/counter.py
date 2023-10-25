94daa19e1a150cfc4f9e63db1048e4fc0e5de705
EmmaDjeufa <57154645+EmmaDjeufa@users.noreply.github.com>
2023-10-25 16:27:36 +0200

    Update counter.py

--- app/counter.py
+++ app/counter.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+import os
 
 app = FastAPI()
 
-@app.get("/", response_class=HTMLResponse)
+# Obtenez le répertoire actuel où se trouve le fichier counter.py
+current_directory = os.path.dirname(os.path.realpath(__file__))
+
+# Construisez le chemin complet du fichier HTML
+html_file_path = os.path.join(current_directory, "ShopifyShop.html")
+
+# Ouvrez le fichier HTML
+with open(html_file_path, "r") as html_file:
+
+#@app.get("/", response_class=HTMLResponse)
 async def read_root():
     with open("ShopifyShop.html", "r") as html_file:
         return html_file.read()
