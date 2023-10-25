1006b49f9531e639b2bf1c08285971d143d2049b
EmmaDjeufa <57154645+EmmaDjeufa@users.noreply.github.com>
2023-10-25 16:55:31 +0200

    Update counter.py

--- app/counter.py
+++ app/counter.py
@@ -1,16 +1,30 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+#!/usr/bin/env python
+from flask import Flask, send_file
+import os
+import redis
 
-app = FastAPI()
+app = Flask(__name__)
 
-@app.get("/", response_class=HTMLResponse)
-async def read_root():
-    try:
-        with open("app/ShopifyShop.html", "r") as html_file:
-            return html_file.read()
-    except FileNotFoundError:
+# Vous pouvez spécifier le nom du service Docker FastAPI dans votre fichier docker-compose.yml.
+# Par exemple, si votre service FastAPI est nommé "app" :
+fastapi_service_name = "app"
+
+# Utilisez le nom du service FastAPI dans l'URL pour accéder à l'API FastAPI depuis le conteneur Flask.
+fastapi_url = f"http://{fastapi_service_name}:5000"
+
+@app.route("/")
+def index():
+    # Définir le chemin du fichier HTML
+    html_file = "ShopifyShop.html"
+
+    # Vérifier si le fichier existe
+    if os.path.exists(html_file):
+        # Utiliser Flask pour renvoyer le fichier HTML
+        return send_file(html_file)
+    else:
         # Afficher un message d'erreur si le fichier n'existe pas
         return "Fichier HTML introuvable", 404
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    # Exécutez Flask avec le port 80, car Flask fonctionnera dans un conteneur séparé.
+    app.run(host="0.0.0.0", port=80)
