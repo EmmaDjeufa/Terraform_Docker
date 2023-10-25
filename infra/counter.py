d12583662b606d45afd0f1e23a8cbbba1e57a7c3
EmmaDjeufa <57154645+EmmaDjeufa@users.noreply.github.com>
2023-10-25 18:53:53 +0200

    Update counter.py

--- infra/counter.py
+++ infra/counter.py
@@ -1,11 +1,8 @@
 import os
 import webbrowser
 
-# Obtenez le chemin absolu du répertoire actuel (où se trouve counter.py)
-current_directory = os.path.dirname(os.path.abspath(__file__))
-
-# Spécifiez le nom du fichier HTML et le chemin relatif vers le dossier "app"
-html_file = os.path.join(current_directory, "app", "ShopifyShop.html")
+# Spécifiez le chemin relatif pour accéder au fichier HTML
+html_file = os.path.join('..', 'app', 'ShopifyShop.html')
 
 # Utilisez la fonction open de webbrowser pour ouvrir le fichier HTML
 webbrowser.open(html_file)
