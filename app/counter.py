8293208a1affe41ca6b001fed53844bfcde57a0a
EmmaDjeufa <57154645+EmmaDjeufa@users.noreply.github.com>
2023-10-25 17:01:57 +0200

    Update counter.py

--- app/counter.py
+++ app/counter.py
@@ -1,16 +1,7 @@
-#!/usr/bin/env python
 from flask import Flask, send_file
 import os
-import redis
 
 app = Flask(__name__)
-
-# Vous pouvez spécifier le nom du service Docker FastAPI dans votre fichier docker-compose.yml.
-# Par exemple, si votre service FastAPI est nommé "app" :
-fastapi_service_name = "app"
-
-# Utilisez le nom du service FastAPI dans l'URL pour accéder à l'API FastAPI depuis le conteneur Flask.
-fastapi_url = f"http://{fastapi_service_name}:5000"
 
 @app.route("/")
 def index():
@@ -26,5 +17,4 @@ def index():
         return "Fichier HTML introuvable", 404
 
 if __name__ == "__main__":
-    # Exécutez Flask avec le port 80, car Flask fonctionnera dans un conteneur séparé.
     app.run(host="0.0.0.0")
