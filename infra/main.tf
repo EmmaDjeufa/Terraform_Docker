bd0a71197e02506364a35c202deb64257543c7d2
EmmaDjeufa <57154645+EmmaDjeufa@users.noreply.github.com>
2023-10-25 15:06:41 +0200

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,22 @@
+#provider "docker" {
+  #host = "unix:///var/run/docker.sock"
+#}
+
+#resource "docker_image" "build" {
+  #name = "nom-de-votre-image"
+  #build {
+    #context = "${path.module}/../app"  # Chemin vers le Dockerfile
+  #}
+#}
+
+#resource "docker_container" "container" {
+  #name = "nom-de-votre-container"
+  #image = docker_image.build.name
+  #ports {
+    #internal = 8000
+  #}
+#}
+
 provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
@@ -5,7 +24,7 @@ provider "docker" {
 resource "docker_image" "build" {
   name = "nom-de-votre-image"
   build {
-    context = "${path.module}/../app"  # Chemin vers le Dockerfile
+    context = "${path.module}/app"  # Chemin vers le Dockerfile
   }
 }
 
@@ -13,6 +32,19 @@ resource "docker_container" "container" {
   name = "nom-de-votre-container"
   image = docker_image.build.name
   ports {
-    internal = 8000
+    internal = 5000  # Port interne de votre application
+  }
+
+  # Vous pouvez ajouter d'autres paramètres ici, par exemple, si vous souhaitez utiliser les variables d'environnement.
+  environment = {
+    DEBUG = "1"
+  }
+
+  # Pour montrer comment utiliser un volume (votre Docker Compose a un volume "www")
+  volumes {
+    container_path = "/src"
+    host_path      = "./www"
+    read_only     = false
   }
 }
+
