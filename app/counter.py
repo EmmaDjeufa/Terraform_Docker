fa689b6255d99c41bfb35fb453276dafecb079b9
EmmaDjeufa <57154645+EmmaDjeufa@users.noreply.github.com>
2023-10-25 16:14:55 +0200

    Update counter.py

--- app/counter.py
+++ app/counter.py
@@ -1,30 +1,10 @@
-#!/usr/bin/env python
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
-from flask import Flask, send_file
-import os
-import redis
+app = FastAPI()
 
-app = Flask(__name__)
+@app.get("/", response_class=HTMLResponse)
+async def read_root():
+    with open("index.html", "r") as html_file:
+        return html_file.read()
 
-redis_host = "redis"
-redis_port = 6379
-
-# Création d'une instance de connexion Redis
-redis_client = redis.StrictRedis(host=redis_host, port=redis_port, db=0)
-
-
-@app.route("/")
-def index():
-    # Définir le chemin du fichier HTML
-    html_file = "ShopifyShop.html"
-
-    # Vérifier si le fichier existe
-    if os.path.exists(html_file):
-        # Utiliser Flask pour renvoyer le fichier HTML
-        return send_file(html_file)
-    else:
-        # Afficher un message d'erreur si le fichier n'existe pas
-        return "Fichier HTML introuvable", 404
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0" , port=5000)
