910241eae9318b6e14dc1002528cbb22ba75f91c
EmmaDjeufa <57154645+EmmaDjeufa@users.noreply.github.com>
2023-10-25 17:16:32 +0200

    Update counter.py

--- app/counter.py
+++ app/counter.py
@@ -5,16 +5,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    # Définir le chemin du fichier HTML
+    # Obtenez le chemin absolu du répertoire actuel
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+
+    # Définir le nom du fichier HTML
     html_file = "ShopifyShop.html"
 
-    # Vérifier si le fichier existe
-    if os.path.exists(html_file):
-        # Utiliser Flask pour renvoyer le fichier HTML
-        return send_file(html_file)
+    # Combinez le chemin du répertoire actuel avec le nom du fichier
+    html_path = os.path.join(current_directory, html_file)
+
+    # Vérifiez si le fichier existe
+    if os.path.exists(html_path):
+        # Utilisez Flask pour renvoyer le fichier HTML
+        return send_file(html_path)
     else:
-        # Afficher un message d'erreur si le fichier n'existe pas
+        # Affichez un message d'erreur si le fichier n'existe pas
         return "Fichier HTML introuvable", 404
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
+
